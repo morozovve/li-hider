94c9960c73ac0b527ac40d1afd076f23daa0237d
Viktor Morozov <v.morozov@samsung.com>
2023-03-19 05:58:25 +0300

    Lot of changes
    Added 'mute' button
    Upd button design
    Impl-d list of muted (stubborn for now)
    Fixed button appearing
    Reworked factories to anon funcs

--- scripts/background.js
+++ scripts/background.js
@@ -11,7 +11,11 @@
 
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
     console.log('Page uses History API and we heard a pushSate/replaceState.');
-    chrome.tabs.sendMessage(details.tabId, {
-        message: 'urlChanged'
-    })
+    console.log(details.tabId);
+    console.log(details.url);
+    if (details.url.match('https:\/\/.*.linkedin.com\/.*')) {
+        chrome.tabs.sendMessage(details.tabId, {
+            message: 'urlChanged'
+        })
+    }
 });
--- scripts/content.js
+++ scripts/content.js
@@ -1,23 +1,26 @@
 let hiddenCnt = 0;
 const setObservers = new Set();
+muteds = Array()
+const elemWhenJobsLoaded = 'div[class*="jobs-search-results-list__pagination"]';
+
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         console.log(`Got msg: ${request.message}`)
         // listen for messages sent from background.js
         if (request.message === 'urlChanged') {
-            if (setObservers.size == 0) {
+            if (setObservers.size == 0 || true) {
                 hiddenCnt = 0;
-                console.log('waiting for element...')
-                waitForElm(document, 'div[class*="jobs-search-results-list__pagination"]').then((elm) => {
+                console.log('waiting for jobs to load...')
+                waitForElm(document, elemWhenJobsLoaded).then((elm) => {
                     console.log('Jobs are ready');
-                    loadJobs();
+                    filterJobs();
                 });
             } else {
                 console.log('repeating request since setObservers.size > 0')
             }
         }
-    });
+});
 
 function waitForElm(obj, selector) {
     return new Promise(resolve => {
@@ -27,6 +30,7 @@ function waitForElm(obj, selector) {
 
         const observer = new MutationObserver(mutations => {
             if (obj.querySelector(selector)) {
+                console.log(`finished loading ${obj.querySelector(selector)}`);
                 resolve(obj.querySelector(selector));
                 observer.disconnect();
             }
@@ -49,71 +53,156 @@ function onVisible(element, callback) {
     setObservers.add(element.id);
 }
 
-function getJobFilterFunc(job) {
-    function filterThisJob() {
-        if (job.style.display == 'none') {
-            if (setObservers.has(job.id)) {
-                setObservers.delete(job.id);
-            }
-            return;
-        }
-        // Extracting data for filtering
-        // Company class: <div> artdeco-entity-lockup__subtitle ember-view
-        // Location class: <div> artdeco-entity-lockup__caption ember-view
-        jobLoc = job.querySelector('div[class*="artdeco-entity-lockup__caption ember-view"]').textContent.trim();
-        jobCompany = job.querySelector('div[class*="artdeco-entity-lockup__subtitle ember-view"]').textContent.trim();
-        console.log("# Processing company:", jobCompany);
-
-        // if matches condition on company/location:
-        if (jobCompany == 'Synsel Techniek') { // TODO add condition
-            job.style.display = 'none';
-            hiddenCnt += 1;
-        }
-
-        counter = document.getElementById('hidden-jobs-counter');
-        if (!counter) {
-            counter = document.createElement("p");
-            counter.setAttribute("id", "hidden-jobs-counter");
-            counter.classList.add("font-weight-light");
-            counter.style.color = "#ffffff";
-            counter.style.fontSize = "12px";
-            counter.textContent = `Hidden jobs: ${hiddenCnt}/25`;
-
-            numRes = document.querySelector('small[class*="jobs-search-results-list__text"]');
-            if (numRes) {
-                numRes.insertAdjacentElement("afterend", counter);
-            } else {
-                alert('not found numRes aka small[class*="jobs-search-results-list__text"]')
-            }
-        } else {
-            // TODO: add link to the ext page like "<a href=??> Job hiding settings </a>"
-            counter.textContent = `Hidden jobs: ${hiddenCnt}/25`;
-        }
-        setObservers.delete(job.id);
-        console.log(`#set: `, setObservers.size);
+// Company class: <div> artdeco-entity-lockup__subtitle ember-view
+function extractCompany(job) {
+    jobCompany = job.querySelector('div[class*="artdeco-entity-lockup__subtitle ember-view"]');
+    if (jobCompany) {
+        return jobCompany.textContent.trim();
+    } else {
+        console.log(`[error]: got empty "jobCompany" from job ${job}`)
+        return '';
     }
-    return filterThisJob;        
 }
 
-function loadJobs() {
+// Location class: <div> artdeco-entity-lockup__caption ember-view
+function extractLocation(job) {
+    jobLoc = job.querySelector('div[class*="artdeco-entity-lockup__caption ember-view"]')
+    if (jobLoc) {
+        return jobLoc.textContent.trim();
+    } else {
+        console.log(`[error]: got empty "jobLocation" from job ${job}`)
+        return '';
+    }
+}
+
+function muteCompany(job) {
+    console.log('clicked mute button');
+    let jobCompany = extractCompany(job);
+    addCompanyToMuted(jobCompany);
+}
+
+function waitAndAddMuteBtn(job) {
+    console.log(`job: ${job.id}`);
+    waitForElm(job, 'button[class*="muted"]').then(() => {
+        console.log('Adding late mute button');
+        console.log(`Found LI mute button: ${job.querySelector('button[class*="muted"]')}`);
+        addMuteBtn(job);
+    });
+}
+
+function addMuteBtn(job) {
+    li_btn = job.querySelector('button[class*="muted"]')
+    if (!li_btn) {
+        return;
+    }
+    muteBtn = document.createElement('button');
+    muteBtn.innerHTML = 'Hide';
+    muteBtn.style.zIndex = "100";
+    muteBtn.classList.add("artdeco-pill--choice");
+    muteBtn.classList.add("artdeco-pill--2");
+    muteBtn.id = 'mute-button-' + job.id;
+    // TODO figure out auto-switching to job even when we trying to hide it
+    // job.style.pointerEvents = "none";
+    // muteBtn.style.pointerEvents = "auto";
+    console.log('adding mute button');
+    muteBtn.onclick = function() {    
+        muteCompany(job);
+        // Or we can just go back when button is clicked so that we're 'staying' on the same job
+        // history.back();
+    };
+    if (!job.querySelector(`button[id="${muteBtn.id}"]`)) {
+        li_btn.insertAdjacentElement("afterend", muteBtn);
+        console.log('added mute button');
+    } else {
+        console.log(`found existing mute button: ${job.querySelector(`button[id="${muteBtn.id}"]`)}`);
+    }
+}
+
+function addCompanyToMuted(companyName) {
+    muteds.push(companyName);
+    console.log(`added company ${companyName} to ${muteds}`);
+}
+
+function updateCounter() {
+    counter = document.getElementById('hidden-jobs-counter');
+    if (!counter) {
+        counter = document.createElement("p");
+        counter.setAttribute("id", "hidden-jobs-counter");
+        counter.classList.add("font-weight-light");
+        counter.style.color = "#ffffff";
+        counter.style.fontSize = "12px";
+        counter.textContent = `Hidden jobs: ${hiddenCnt}/25`;
+
+        numRes = document.querySelector('small[class*="jobs-search-results-list__text"]');
+        if (numRes) {
+            numRes.insertAdjacentElement("afterend", counter);
+        } else {
+            alert('not found numRes')
+        }
+    } else {
+        // TODO: add link to the ext page like "<a href=??> Job hiding settings </a>"
+        counter.textContent = `Hidden jobs: ${hiddenCnt}/25`;
+    }
+}
+
+function filterJob(job) {
+    let res = 0;
+    if (job.style.display == 'none') {
+        if (setObservers.has(job.id)) {
+            setObservers.delete(job.id);
+        }
+        return 1;
+    }
+    // Extracting data for filtering
+    jobLoc = extractLocation(job)
+    jobCompany = extractCompany(job)
+    console.log("# Processing company:", jobCompany);
+
+    // if matches condition on company/location:
+    if (muteds.includes(jobCompany)) { // TODO add condition
+        job.style.display = 'none';
+        res = 1;
+    }
+    setObservers.delete(job.id);
+    console.log(`#set: `, setObservers.size);
+    return res;
+}
+
+function filterJobs() {
     const jobs = document.body.querySelectorAll('li[class*="jobs-search-results__list-item"]');
     for (var i=0, max=jobs.length; i < max; i++) {
         if (jobs[i].textContent && jobs[i].textContent.trim()) {
             if (jobs[i].style.display !== 'none') {
                 console.log(`Ready to filter: ${i}`);
-                getJobFilterFunc(jobs[i])();
+                addMuteBtn(jobs[i]);
+                hiddenCnt += filterJob(jobs[i]);
+                updateCounter();
+            } else {
+                hiddenCnt += 1;
+                updateCounter();
             }
         } else {
             if (!setObservers.has(jobs[i].id)) {
-                console.log(`Set callback: ${i}`);
-                onVisible(jobs[i], getJobFilterFunc(jobs[i]));
+                let job = jobs[i];
+                console.log(`Set callback: ${i} for job ${job.id}`);
+                let addfun = function() {
+                    waitAndAddMuteBtn(job);
+                };
+                let filterfun = function() {
+                    hiddenCnt += filterJob(job);
+                }
+                onVisible(job, function() {
+                    addfun();
+                    filterfun();
+                    updateCounter();
+                });
             }
         }
     }
 }
 
-waitForElm(document, 'div[class*="jobs-search-results-list__pagination"]').then((elm) => {
+waitForElm(document, elemWhenJobsLoaded).then((elm) => {
     console.log('Jobs are ready');
-    loadJobs();
+    filterJobs();
 });
 
